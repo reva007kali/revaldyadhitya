d3c16922f6c253ccfe5bca93344307613effb81a
reva007kali <le.revaldy@gmail.com>
2025-11-06 05:27:31 +0700

    kjhk

--- resources/views/livewire/cv-form.blade.php
+++ resources/views/livewire/cv-form.blade.php
@@ -582,30 +582,3 @@
     </form>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
-<script>
-    const swiper = new Swiper(".mySwiper", {
-        slidesPerView: 1,
-        spaceBetween: 20,
-        grabCursor: true,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        breakpoints: {
-            640: {
-                slidesPerView: 2,
-                spaceBetween: 20
-            },
-            1024: {
-                slidesPerView: 3,
-                spaceBetween: 30
-            },
-        },
-        preventClicks: true, // penting supaya klik card tetap jalan
-        preventClicksPropagation: true,
-    });
-</script>
