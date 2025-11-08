31c52765d01483bddccb3d32d8974b809ca76067
reva007kali <le.revaldy@gmail.com>
2025-11-09 03:55:02 +0700

    update

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>{{ $title ?? 'CV Maker' }}</title>
+    <link rel="icon" href="/favicon.png" type="image/svg+xml">
+
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
 
